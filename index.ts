21c6322a13fe7e064dc102b2052d881d9cad3fca
Rajil Bajracharya <imrb1213@gmail.com>
2022-11-07 01:30:33 +0545

    Add types to index.js

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
-'use strict';
+import { assert } from "console";
+import Joi from 'joi';
 
-var assert = require('assert');
-var defaultMessage = 'valid mongo id';
+let defaultMessage = 'valid mongo id';
+type joiType = typeof Joi;
 
-module.exports = function joiObjectId(Joi, message) {
-  assert(Joi && Joi.isJoi, 'you must pass Joi as an argument');
-  if (message == undefined) {
-    message = defaultMessage;
-  }
+export const joiObjectId = (Joi: joiType, message?: string) => {
+  assert(Joi && Joi.object, 'you must pass Joi as an argument');
+  if (!message || !(typeof message === 'string')) {
+        message = defaultMessage;
+    }
   return function objectId() {
     return Joi.string().regex(/^[0-9a-fA-F]{24}$/, message);
   };
